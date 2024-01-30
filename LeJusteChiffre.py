26380409fea7ce268d3da40f4293d9166d5f6f1e
Xorgaz <158171268+Xorgaz@users.noreply.github.com>
2024-01-30 16:23:23 +0100

    LeJusteChiffre.py

--- LeJusteChiffre.py
+++ LeJusteChiffre.py
@@ -1,14 +1,34 @@
 # Le Juste Chiffre
 # Le but du jeu est de trouver le chiffre généré aléatoirement.
 
-import re
+import random
 
-def translate(text):
-    """Translate the given text to Parseltongue.
-
-    :param str text: The text to translate.
-
-    :returns: the translated text.
-    :rtype: str
+def generer_nombre():
     """
-    return re.sub(r"[\w\d]", "s", text)
+    Retourne un nombre aléatoire entre 0 et 100.
+    """
+    return random.randint(0, 100)
+
+def jouer():
+    """
+    Gère le jeu LeJusteChiffre.
+    """
+    # Générer un nombre aléatoire.
+    nombre_secret = generer_nombre()
+
+    # Demander à l'utilisateur de deviner le nombre.
+    nombre_propose = input("Devinez le nombre : ")
+
+    # Comparer le nombre proposé au nombre secret.
+    while nombre_propose != nombre_secret:
+        if nombre_propose < nombre_secret:
+            print("Le nombre est plus grand.")
+        elif nombre_propose > nombre_secret:
+            print("Le nombre est plus petit.")
+        nombre_propose = input("Devinez à nouveau : ")
+
+    # Féliciter l'utilisateur s'il a trouvé le bon nombre.
+    print("Bravo ! Vous avez trouvé le nombre secret.")
+
+if __name__ == "__main__":
+    jouer()
